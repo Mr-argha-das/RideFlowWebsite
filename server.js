8eaf66d3013ce7e257ebee676d1194f1a95b0a6b
Mr-argha-das <81897206+Mr-argha-das@users.noreply.github.com>
2025-01-03 22:11:38 +0530

    Update server.js
    sadad

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 // Import the routes
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
